0c847fcbcc93fbbe40641f63d4b243f1e605c5a5
Fahmi Jabbar <rootmawar77@gmail.com>
2022-03-23 20:03:27 +0700

    Handson 2-2

---
+++ src/components/AppImage/index.js
@@ -0,0 +1,5 @@
+const AppImage = ({ url, title }) => {
+    return <img src={url} alt={title} />;
+};
+
+export default AppImage;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,24 @@
-import HomeButton from "./HomeButton";
-import HomeInput from "./HomeInput";
-import HomeImage from "./HomeImage";
+import AppButton from "components/AppButton";
+import AppImage from "components/AppImage";
+import AppInput from "components/AppInput";
 
 const Index = () => {
+    const gif = {
+        id: "4HrBfVJJveBNS9ytSk",
+        title: "Nintendo Plotting GIF by Gaming GIFs",
+        uploadedDate: "2018-04-03 15:21:50",
+        url: "https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/200w.gif?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=200w.gif&ct=g",
+        webp: "https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/giphy.webp?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=giphy.webp&ct=g",
+    };
+
     return (
         <>
             <form>
-                <HomeInput></HomeInput>
-                <HomeButton></HomeButton>
+                <AppInput></AppInput>
+                <AppButton></AppButton>
             </form>
             <div className="App-Image">
-                <HomeImage></HomeImage>
+                <AppImage url={gif.url} title={gif.title}></AppImage>
             </div>
         </>
     );
